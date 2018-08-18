2e63e2150b85e16d965e0ceda581318451123641
Daniel Totten <TottenDC@gmail.com>
2018-08-18 17:46:18 -0400

    Final polish
    -Add extra tag to quotes (episode, if applicable).
    -Update the conditional in background color function to change text to black (and update to the near-white background) if it goes near white.
    -clean up the call to change CSS elements by removing the body id.
    -clean the printQuote conditionals by just telling it to find the property and display.
    -Move timer to printQuote function and clear it each time it is called before declaring a new one.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 Random Quote Generator Program by Daniel Totten
 */
 
-// Create the array of quote objects and name it quotes
+// create updatable array of quote objects
 var quotes = [
   {
     quote: 'As far as I\'m concerned, if something is so complicated that you can\'t explain it in 10 seconds, then it\'s probably not worth knowing anyway.',
@@ -15,6 +15,7 @@ var quotes = [
     quote: 'People waste their time pondering whether the glass is half empty or half full. Me? I just drink whatever\'s in the glass.',
     source: 'Sophia Petrillo (Estelle Getty)',
     citation: 'The Golden Girls',
+    episode: 'Season 5, Episode 14',
     year: 1990
   },
   {
@@ -39,18 +40,21 @@ var quotes = [
     quote: 'If that\'s a veiled criticism about me, I won\'t hear it and I won\'t respond to it.',
     source: 'Lucille Bluth (Jessica Walter)',
     citation: 'Arrested Development',
+    episode: 'Season 1, Episode 4',
     year: 2003
   },
   {
     quote: 'It\'s easy to do nothing, it\'s hard to forgive.',
     source: 'Aang (Zach Tyler Eisen)',
     citation: 'Avatar: The Last Airbender',
+    episode: 'Season 3, Episode 16',
     year: 2008
   },
   {
     quote: 'Life happens wherever you are, whether you make it or not.',
     source: 'Iroh (Mako Iwamatsu)',
     citation: 'Avatar: The Last Airbender',
+    episode: 'Season 2, Episode 14',
     year: 2006
   },
   {
@@ -66,8 +70,9 @@ var quotes = [
     year: 2013
   }
 ];
+// create an empty timer variable that will be used for auto-updating the page after pressing the button
+var timer;
 
-// Create the getRandomQuote function
 /*
 getRandomQuote function
 1. creates a random number to access an index value in quotes array
@@ -83,8 +88,8 @@ randomBackgroundColor Function
 1. create function to return random value from 0-255
 2. create random red, green, and blue values using the random value Function
 3. create rgb(#,#,#) string
-4. access body element and button element to change CSS
-5. ask if the color combination is getting too close to white. If it is, re-randomize the values.
+4. access button element to change CSS
+5. ask if the color combination is getting too close to white. If it is, set text to black.
 6. if not, set background and button color to new, random string
 */
 function randomColorValue() {
@@ -96,19 +101,19 @@ function randomBackgroundColor() {
   var blue = randomColorValue();
 
   var colorString = 'rgb(' + red + ',' + green + ',' + blue + ')';
-
-  var bodyElem = document.getElementById('page');
   var buttonElem = document.getElementById('loadQuote');
-  if (red >=200 && blue >= 200 && green >= 200) {
-    red = randomColorValue();
-    green = randomColorValue();
-    blue = randomColor();
+
+  if (red >=150 && blue >= 150 && green >= 150) {
+    document.body.style.color = 'black';
+    document.body.style.backgroundColor = colorString;
+    buttonElem.style.backgroundColor = colorString;
   } else {
-    bodyElem.style.backgroundColor = colorString;
+    document.body.style.color = 'white';
+    document.body.style.backgroundColor = colorString;
     buttonElem.style.backgroundColor = colorString;
   }
 }
-// Create the printQuote funtion
+
 /*
 printQuote Function
 1. generates named object that holds a randomly-generated object from the quotes array
@@ -116,27 +121,33 @@ printQuote Function
 2a. conditionals check for extra properties in the object and adds to the string
 3. prints string to the page in the div element
 4. changes the background and button color
+5. clears the previous timer interval (if present), then starts new timer interval (10 seconds)
 */
 function printQuote() {
   var quote = getRandomQuote(quotes);
 
   var quotePropertiesToPrint = '<p class="quote">' + quote.quote + '</p>';
   quotePropertiesToPrint += '<p class="source">' + quote.source;
-  if ( quote.citation !== ' ' ) {
+  if ( quote.citation ) {
     quotePropertiesToPrint += '<span class="citation">' + quote.citation + '</span>';
   }
-  if ( quote.year !== ' ' ) {
+  if ( quote.episode ) {
+    quotePropertiesToPrint += '<span class="episode">' + quote.episode + '</span>';
+  }
+  if ( quote.year ) {
     quotePropertiesToPrint += '<span class="year">' + quote.year +'</span>';
   }
   quotePropertiesToPrint += '</p>';
 
   document.getElementById('quote-box').innerHTML = quotePropertiesToPrint;
   randomBackgroundColor();
+
+  clearInterval(timer);
+  timer = window.setInterval(printQuote, 10000);
+
 }
 
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-var intervalID = window.setInterval(printQuote, 10000);
