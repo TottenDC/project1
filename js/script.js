49caafb8b49701393396d6ef87c1667c0556728a
Daniel Totten <TottenDC@gmail.com>
2018-08-18 11:24:38 -0400

    Created functions for 'pass' requirements

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ var quotes = [
   {
     quote: '"As far as I\'m concerned, if something is so complicated that you can\'t explain it in 10 seconds, then it\'s probably not worth knowing anyway."',
     source: 'Bill Waterson',
-    citation: '<em>The Indispensable Calivn and Hobbes</em>',
+    citation: 'The Indispensable Calivn and Hobbes',
     year: 1992
   },
   {
     quote: '"People waste their time pondering whether the glass is half empty or half full. Me? I just drink whatever\'s in the glass."',
     source: 'Sophia Petrillo (Estelle Getty)',
-    citation: '<em>The Golden Girls</em>',
+    citation: 'The Golden Girls',
     year: 1990
   },
   {
@@ -26,55 +26,74 @@ var quotes = [
   {
     quote: '"The thing is - fear can\'t hurt you any more than a dream."',
     source: 'William Golding',
-    citation: '<em>Lord of the Files</em>',
+    citation: 'Lord of the Flies',
     year: 1954
   },
   {
     quote: '"Words can be like X-rays if you use them properly -- they\'ll go through anything. You read and you\'re pierced."',
     source: 'Aldous Huxley',
-    citation: '<em>Brave New World</em>',
+    citation: 'Brave New World',
     year: 1932
   },
   {
     quote: '"If that\'s a veiled criticism about me, I won\'t hear it and I won\'t respond to it."',
     source: 'Lucille Bluth (Jessica Walter)',
-    citation: '<em>Arrested Development</em>',
+    citation: 'Arrested Development',
     year: 2003
   },
   {
     quote: '"It\'s easy to do nothing, it\'s hard to forgive."',
     source: 'Aang (Zach Tyler Eisen)',
-    citation: '<em>Avatar: The Last Airbender</em>',
+    citation: 'Avatar: The Last Airbender',
     year: 2008
   },
   {
     quote: '"Life happens wherever you are, whether you make it or not."',
     source: 'Iroh (Mako Iwamatsu)',
-    citation: '<em>Avatar: The Last Airbender</em>',
+    citation: 'Avatar: The Last Airbender',
     year: 2006
   },
   {
     quote: '"We all make choices, but in the end, our choices make us."',
     source: 'Andrew Ryan (Armin Shimerman)',
-    citation: '<em>BioShock</em>',
+    citation: 'BioShock',
     year: 2007
   },
   {
     quote: '"Time... Time rots everything. Even hope."',
     source: 'Elizabeth DeWitt Comstock (Courtnee Draper)',
-    citation: '<em>BioShock Infinite</em>',
+    citation: 'BioShock Infinite',
     year: 2013
   }
 ];
 
-// Create the getRandomQuuote function and name it getRandomQuote
+// Create the getRandomQuote function
+/*
+getRandomQuote function
+1. creates a random number to access an index value in quotes array
+2. returns the object at a random index position
+*/
+function getRandomQuote(array) {
+  var randomIndex = Math.floor(Math.random() * (array.length + 1));
+  return array[randomIndex];
+}
 
-
-
-// Create the printQuote funtion and name it printQuote
-
+// Create the printQuote funtion
+function printQuote() {
+  var quote = getRandomQuote(quotes);
+  var quotePropertiesToPrint = '<p class="quote">' + quote.quote + '</p>';
+  quotePropertiesToPrint += '<p class="source">' + quote.source;
+  if ( quote.citation !== ' ' ) {
+    quotePropertiesToPrint += '<span class="citation">' + quote.citation + '</span>';
+  }
+  if ( quote.year !== ' ' ) {
+    quotePropertiesToPrint += '<span class="year">' + quote.year +'</span>';
+  }
+  quotePropertiesToPrint += '</p>'
+  return quotePropertiesToPrint;
+}
 
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote(), false);
