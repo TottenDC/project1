2b193b588e026def84559dd1dff4c3348f720a60
Daniel Totten <TottenDC@gmail.com>
2018-08-18 10:42:34 -0400

    Input quotes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,71 @@
 // FSJS - Random Quote Generator
+/*
+Random Quote Generator Program by Daniel Totten
+*/
 
 // Create the array of quote objects and name it quotes
-
-
+var quotes = [
+  {
+    quote: '"As far as I\'m concerned, if something is so complicated that you can\'t explain it in 10 seconds, then it\'s probably not worth knowing anyway."',
+    source: 'Bill Waterson',
+    citation: '<em>The Indispensable Calivn and Hobbes</em>',
+    year: 1992
+  },
+  {
+    quote: '"People waste their time pondering whether the glass is half empty or half full. Me? I just drink whatever\'s in the glass."',
+    source: 'Sophia Petrillo (Estelle Getty)',
+    citation: '<em>The Golden Girls</em>',
+    year: 1990
+  },
+  {
+    quote: '"Man is least himself when he talks in his own person. Give him a mask, and he will tell you the truth."',
+    source: 'Oscar Wilde',
+    citation: '"The True Function and Value of Criticism"',
+    year: 1890
+  },
+  {
+    quote: '"The thing is - fear can\'t hurt you any more than a dream."',
+    source: 'William Golding',
+    citation: '<em>Lord of the Files</em>',
+    year: 1954
+  },
+  {
+    quote: '"Words can be like X-rays if you use them properly -- they\'ll go through anything. You read and you\'re pierced."',
+    source: 'Aldous Huxley',
+    citation: '<em>Brave New World</em>',
+    year: 1932
+  },
+  {
+    quote: '"If that\'s a veiled criticism about me, I won\'t hear it and I won\'t respond to it."',
+    source: 'Lucille Bluth (Jessica Walter)',
+    citation: '<em>Arrested Development</em>',
+    year: 2003
+  },
+  {
+    quote: '"It\'s easy to do nothing, it\'s hard to forgive."',
+    source: 'Aang (Zach Tyler Eisen)',
+    citation: '<em>Avatar: The Last Airbender</em>',
+    year: 2008
+  },
+  {
+    quote: '"Life happens wherever you are, whether you make it or not."',
+    source: 'Iroh (Mako Iwamatsu)',
+    citation: '<em>Avatar: The Last Airbender</em>',
+    year: 2006
+  },
+  {
+    quote: '"We all make choices, but in the end, our choices make us."',
+    source: 'Andrew Ryan (Armin Shimerman)',
+    citation: '<em>BioShock</em>',
+    year: 2007
+  },
+  {
+    quote: '"Time... Time rots everything. Even hope."',
+    source: 'Elizabeth DeWitt Comstock (Courtnee Draper)',
+    citation: '<em>BioShock Infinite</em>',
+    year: 2013
+  }
+];
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
